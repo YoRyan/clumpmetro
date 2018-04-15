8bae0b424d2f958b8732474999fd6cdda85b3d9a
Ryan Young <rayoung@utexas.edu>
2018-04-15 16:43:52 -0500

    Modify script to track and record arrival events

--- record_arrivals.py
+++ record_arrivals.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-# use me: ./record_predictions.py 559 4039 591 610 2821 >dump.csv
+# use me: ./record_arrivals.py 559 4039 591 610 2821 >dump.csv
 
 import json
 import sys
@@ -28,20 +28,38 @@ def departures_for_stop(trip_updates, stop_id):
                      for entity in trip_updates['entity']
                      if len(stop_time_updates(entity)) == 1]
     return {datetime.fromtimestamp(stu['arrival']['time']): trip
-            for (trip, stu) in inbound_trips if 'arrival' in stu}
+            for (trip, stu) in inbound_trips if stu is not None}
 
 if __name__ == '__main__':
     STOP_IDS = sys.argv[1:]
 
-    print('updated', 'stop id', 'predicted arrival', 'route', 'trip id', sep=',')
+    print('stop id', 'arrival time', 'route', 'trip id', sep=',')
+    seen_routes = {stop: {} for stop in STOP_IDS}
+    seen_trips = {stop: set() for stop in STOP_IDS}
     while True:
         data = get_trip_updates()
         retrieved = datetime.fromtimestamp(data['header']['timestamp'])
+
         for stop in STOP_IDS:
-            for (t, d) in departures_for_stop(data, stop).items():
-                print(retrieved.isoformat(timespec='seconds'), stop,
-                      t.isoformat(timespec='minutes'), d['route_id'], d['trip_id'],
+            departures = [d for (_, d) in departures_for_stop(data, stop).items()]
+            future_trips = set([d['trip_id'] for d in departures])
+
+            # seen before, now gone
+            for departed_trip in seen_trips[stop] - future_trips:
+                print(stop,
+                      retrieved.isoformat(timespec='seconds'),
+                      seen_routes[stop][departed_trip],
+                      departed_trip,
                       sep=',')
+                seen_trips[stop].remove(departed_trip)
+                seen_routes[stop].pop(departed_trip)
+
+            # not seen before
+            for future_trip in future_trips - seen_trips[stop]:
+                departure = [d for d in departures if d['trip_id'] == future_trip][0]
+                seen_trips[stop].add(future_trip)
+                seen_routes[stop][future_trip] = departure['route_id']
+
         sys.stdout.flush()
         sleep(30)
 
